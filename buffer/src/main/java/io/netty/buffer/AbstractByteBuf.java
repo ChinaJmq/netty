bf3a8eed802d2aae905809dd1c8f7e9728a9fde7
jimingqiang <476747225@qq.com>
2018-08-16 15:27:12 +0800

    测试git03

--- buffer/src/main/java/io/netty/buffer/AbstractByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/AbstractByteBuf.java
@@ -40,7 +40,7 @@ import static io.netty.util.internal.MathUtil.isOutOfBounds;
 
 /**
  * A skeletal implementation of a buffer.
- * xxxxxxxyyyyyyzzzz
+ * xxxxxxxyyyyyyzzzzuuu
  */
 public abstract class AbstractByteBuf extends ByteBuf {
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(AbstractByteBuf.class);
